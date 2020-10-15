d28bd91f68c91aa282494eddcfe766220c1cac70
Marvin Filus <marvinfilus08@gmail.com>
2020-10-15 06:32:43 -0400

    working on it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user : {}
+    }
+  }
+  render(){
+    console.log('hello')
+    return (
+      <div className="App">
+        <h1> Instagram </h1>
+        <header className="App-header">
+          <div className="div">
+            <p> Log In </p>
+            <input type="text" />
+            <p> Password</p>
+            <input type="password" />
+            <br/>
+            <button> Submit</button>
+          </div>
+          <div className="div">
+            <p> Sign Up </p>
+            <input type="text" />
+            <p> Password</p>
+            <input type="password" />
+            <br/>
+            <button> Submit</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
